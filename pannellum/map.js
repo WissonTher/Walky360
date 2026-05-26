b27b95ac1271077222194448b66d965747fdb06f
M4c10S <maciek.tankspl@gmail.com>
2026-05-26 16:55:26 +0200

    aktualizacja

--- pannellum/map.js
+++ pannellum/map.js
@@ -52,3 +52,190 @@ markerSK1.on('click', function () {
     }
 })
 markeryPietroI.addLayer(markerSK1);
+
+const markerSK5 = L.marker([702, 354]).addTo(map);
+markerSK5.bindPopup("Sala koncertowa 05");
+markerSK5.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("05-koncertowa")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSK5);
+
+const markerSK2 = L.marker([845, 189]).addTo(map);
+markerSK2.bindPopup("Sala Koncertowa 02");
+markerSK2.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("02-koncertowa")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSK2);
+
+const markerSK3 = L.marker([812, 264]).addTo(map);
+markerSK3.bindPopup("Sala Koncertowa 03");
+markerSK3.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("03-koncertowa")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSK3);
+
+const markerSK6 = L.marker([744, 282]).addTo(map);
+markerSK6.bindPopup("Sala Koncertowa 06");
+markerSK6.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("06-koncertowa")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSK6);
+
+const markerSK4 = L.marker([675, 265]).addTo(map);
+markerSK4.bindPopup("Sala Koncertowa 04");
+markerSK4.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("04-koncertowa")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSK4);
+
+const markerST1 = L.marker([1261, 914]).addTo(map);
+markerST1.bindPopup("Sala Tronowa 01");
+markerST1.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("01-tronowa")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerST1)
+
+const markerST2 = L.marker([1239, 863]).addTo(map);
+markerST2.bindPopup("Sala Tronowa 02");
+markerST2.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("02-tronowa")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerST2);
+
+const markerST3 = L.marker([1136, 909]).addTo(map);
+markerST3.bindPopup("Sala Tronowa 03");
+markerST3.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("03-tronowa")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerST3);
+
+const markerST4 = L.marker([1158, 952]).addTo(map);
+markerST4.bindPopup("Sala Tronowa 04");
+markerST4.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("04-tronowa")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerST4);
+
+const markerSS1 = L.marker([280, 899]).addTo(map);
+markerSS1.bindPopup("Sala Senacka 01");
+markerSS1.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("01-senacka")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSS1);
+
+const markerSS2 = L.marker([162, 904]).addTo(map);
+markerSS2.bindPopup("Sala Senacka 02");
+markerSS2.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("02-senacka")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSS2);
+
+const markerSS3 = L.marker([228, 862]).addTo(map);
+markerSS3.bindPopup("Sala Senacka 03");
+markerSS3.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("03-senacka")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSS3);
+
+const markerSS4 = L.marker([236, 801]).addTo(map);
+markerSS4.bindPopup("Sala Senacka 04");
+markerSS4.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("04-senacka")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSS4);
+
+const markerSS5 = L.marker([255, 743]).addTo(map);
+markerSS5.bindPopup("Sala Senacka 05");
+markerSS5.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("05-senacka")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSS5);
+
+const markerSS6 = L.marker([226, 723]).addTo(map);
+markerSS6.bindPopup("Sala Senacka 06");
+markerSS6.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("06-senacka")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSS6);
+
+const markerSS7 = L.marker([168, 682]).addTo(map);
+markerSS7.bindPopup("Sala Senacka 07");
+markerSS7.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("07-senacka")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSS7);
+
+const markerSS8 = L.marker([273, 676]).addTo(map);
+markerSS8.bindPopup("Sala Senacka 08");
+markerSS8.on('click', function () {
+    if (window.viewer) {
+        window.viewer.loadScene("08-senacka")
+    } else {
+        console.error("System Leaflet: Brak połączenia z głównym silnikiem Pannellum.");
+    }
+})
+markeryPietroI.addLayer(markerSS8);
